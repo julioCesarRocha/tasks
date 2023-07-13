3333a5fac975123fc39c864d13cacfbee89779a1
Julio Cesar <julio.junior1@estudante.ifb.edu.br>
2023-07-13 13:21:54 -0300

    feat: Terminando implementação do crud

--- tasks_manager/tasks/views.py
+++ tasks_manager/tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
 from .forms import TaskForm
+from django.http import HttpResponse
 
 def task_list(request):
     tasks = Task.objects.all()
@@ -15,7 +16,7 @@ def task_create(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             task = form.save()
-            return redirect('tasks:task_detail', pk=task.pk)
+            return redirect('tasks:task_list')
     else:
         form = TaskForm()
     return render(request, 'tasks/task_form.html', {'form': form})
@@ -35,5 +36,5 @@ def task_delete(request, pk):
     task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
         task.delete()
-        return redirect('tasks:task_list')
-    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
+        return HttpResponse(status=204)  # Retorna uma resposta vazia
+    return HttpResponse(status=400)
